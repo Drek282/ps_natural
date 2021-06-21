b4a9c3c1bd80c831cc25e899c089bab8e25c5575
Drek <doxology@rogers.com>
2021-06-21 15:01:38 -0300

    Various fixes for related globals and directories.

--- www/includes/PS/halflife/natural.php
+++ www/includes/PS/halflife/natural.php
@@ -9,7 +9,7 @@ if (!defined("PSYCHOSTATS_PAGE")) die("Unauthorized access to " . basename(__FIL
 if (defined("CLASS_PS_HALFLIFE_NATURAL_PHP")) return 1;
 define("CLASS_PS_HALFLIFE_NATURAL_PHP", 1);
 
-include_once(dirname(__DIR__) . '/halflife.php');
+include_once(rtrim(dirname(__DIR__), '/\\') . '/halflife.php');
 
 class PS_halflife_natural extends PS_halflife {
 
